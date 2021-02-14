1f1faa937c2dd160c7ce6e3fe6814a7bebe8884e
Rafael Bernabeu <rbbernabeu@gmail.com>
2021-02-14 04:05:28 +0000

    rotas e links para novos modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,30 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './components/home/home.component';
 import {UsuarioComponent} from './components/usuario/usuario.component';
 import {EntidadeComponent} from './components/entidade/entidade.component';
+import {CotacaoComponent} from './components/cotacao/cotacao.component';
+import {ProdutoComponent} from './components/produto/produto.component';
+import {HospitalComponent} from './components/hospital/hospital.component';
+import {LaboratorioComponent} from './components/laboratorio/laboratorio.component';
+import {ProfissaoComponent} from './components/profissao/profissao.component';
+import {OperadoraComponent} from './components/operadora/operadora.component';
+import {AdministradoraComponent} from './components/administradora/administradora.component';
+import {AuditoriaComponent} from './components/auditoria/auditoria.component';
 
 const routes: Routes = [
 
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'cotacao', component: CotacaoComponent },
   { path: 'usuarios', component: UsuarioComponent },
+  { path: 'produtos', component: ProdutoComponent },
   { path: 'entidades', component: EntidadeComponent },
+  { path: 'hospitais', component: HospitalComponent },
+  { path: 'auditoria', component: AuditoriaComponent },
+  { path: 'profissoes', component: ProfissaoComponent },
+  { path: 'operadoras', component: OperadoraComponent },
+  { path: 'laboratorios', component: LaboratorioComponent },
+  { path: 'administradora', component: AdministradoraComponent },
 
 ];
 
