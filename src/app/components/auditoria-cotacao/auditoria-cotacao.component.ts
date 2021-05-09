7f52eac6a7106421946dd070c5f9739aeaf125b5
Rafael Bernabeu <rbbernabeu@gmail.com>
2021-05-09 01:23:39 +0000

    Trabalhando em Auditorias (Cotaçao)

--- src/app/components/auditoria-cotacao/auditoria-cotacao.component.ts
+++ src/app/components/auditoria-cotacao/auditoria-cotacao.component.ts
@@ -1,4 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatTableDataSource} from "@angular/material/table";
+import {AuditoriaLogin} from "../../services/auditoria/auditoria-login";
+import {FormControl, FormGroup} from "@angular/forms";
+import {AuditoriaService} from "../../services/auditoria/auditoria.service";
+import {UtilService} from "../../services/util/util.service";
+import {AuditoriaCotacao} from "../../services/auditoria/auditoria-cotacao";
 
 @Component({
   selector: 'app-auditoria-cotacao',
@@ -7,9 +15,46 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AuditoriaCotacaoComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatSort) sortLogin: MatSort;
+  @ViewChild(MatPaginator) paginatorLogin: MatPaginator;
+
+  displayedColumns: string[] = ["dataHora", "usuario", "link"];
+  dataSourceCotacao = new MatTableDataSource<AuditoriaCotacao>();
+
+  dataAtual = new Date();
+  datasSelecionadas = new FormGroup({
+    dataInicio: new FormControl(new Date(this.dataAtual.getTime() - (60 * 60 * 24 * 30 * 1000))),
+    dataFim: new FormControl(this.dataAtual)
+  });
+
+  constructor(
+    private auditoriaService: AuditoriaService,
+  ) { }
 
   ngOnInit(): void {
+    this.carregaCotacoes();
+  }
+
+  carregaCotacoes() {
+    if (this.datasSelecionadas.valid) {
+      this.auditoriaService.getAllCotacoes(this.datasSelecionadas.value).subscribe(response => {
+        this.dataSourceCotacao = new MatTableDataSource<AuditoriaCotacao>(response);
+        this.dataSourceCotacao.sort = this.sortLogin;
+        this.dataSourceCotacao.paginator = this.paginatorLogin;
+        this.dataSourceCotacao.sortingDataAccessor = (login, property) => {
+          switch (property) {
+            case 'dataHora':
+              return login[property];
+            default:
+              return login[property];
+          }
+        }
+      })
+    }
+  }
+
+  getDate(dataHora: any) {
+    return UtilService.getDate(dataHora);
   }
 
 }
--- src/app/services/auditoria/auditoria-cotacao.ts
+++ src/app/services/auditoria/auditoria-cotacao.ts
@@ -1,6 +1,9 @@
+import {Usuario} from "../usuario/usuario";
 
 export class AuditoriaCotacao {
 
-
+  id: number;
+  dataHora: Date;
+  usuario: Usuario;
 
 }
